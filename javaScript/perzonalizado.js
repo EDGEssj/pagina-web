04731dc2a2dd4f3ce198ff38371a010bd15c70af
EDGEssj <93457595+EDGEssj@users.noreply.github.com>
2021-10-30 19:58:05 -0600

    Add files via upload

---
+++ javaScript/perzonalizado.js
@@ -0,0 +1,23 @@
+$( document ).ready(function(){
+    $(".dropdown-trigger").dropdown();
+});
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
